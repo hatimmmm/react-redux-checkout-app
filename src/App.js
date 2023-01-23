510f332f2ad491b45f0c6fbfaa4e7bd8bb846775
hatim <ourachhatim0@gmail.com>
2023-01-23 21:12:59 +0100

    adding a product carousel

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import "./App.css";
 import React from "react";
-import AddToCart from "./components/AddToCart";
+// import AddToCart from "./components/AddToCart";
 import Cart from "./components/Cart";
 import Total from "./components/Total";
 import { useSelector } from "react-redux";
+import Slider from "./components/card-slider/Slider";
 
 const App = () => {
-  const { data } = useSelector((state) => state.cart);
-  console.log(data);
   return (
     <div className="container">
-      <AddToCart />
+      <Slider />
       <Cart />
       <Total />
     </div>
--- src/components/card-slider/Slider.js
+++ src/components/card-slider/Slider.js
@@ -1,15 +1,88 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import { setItems } from "../../store/features/cart/cartSlice";
+import { v4 as uuidv4 } from "uuid";
 
-export const Slideshow = () => {
+const Slider = ({ deviceType }) => {
   const { data } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
+
+  const onClick = (e, name) => {
+    e.preventDefault();
+    for (let x in data) {
+      if (data[x].name === name) {
+        let id = uuidv4();
+        let newItem = { id: id, ...data[x] };
+        console.log(newItem);
+        dispatch(setItems(newItem));
+      }
+    }
+  };
+
   return (
-    <div className="wrapper">
-      <div className="slider">
-        {data.map((item)=>{
-            return()
-            })}
-      </div>
-    </div>
+    <Carousel
+      swipeable={true}
+      draggable={true}
+      showDots={false}
+      responsive={responsive}
+      ssr={true} // means to render carousel on server-side.
+      infinite={true}
+      // autoPlay={deviceType !== "mobile" ? true : false}
+      autoPlaySpeed={1000}
+      keyBoardControl={true}
+      // customTransition="all .5"
+      // transitionDuration={500}
+      containerClass="carousel-container"
+      removeArrowOnDeviceType={["tablet", "mobile"]}
+      deviceType={deviceType}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+    >
+      {data.map((item) => {
+        return (
+          <div className="card-container" key={item.name}>
+            <div className="card-img">
+              <img src={item.image} alt={item.name} />
+            </div>
+            <div className="card-name">
+              <p>{item.name}</p>
+            </div>
+            <div className="price-size">
+              <div className="card-size">{item.size}"</div>
+              <div className="card-price">{item.price}.00DH</div>
+            </div>
+            <div className="add-to-cart">
+              <button type="button" onClick={(e) => onClick(e, item.name)}>
+                add to cart
+              </button>
+            </div>
+          </div>
+        );
+      })}
+    </Carousel>
   );
 };
+
+export default Slider;
