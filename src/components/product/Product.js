adce6c9b5def77eb1cc9e676a75c582df0cf574a
hatim <ourachhatim0@gmail.com>
2023-02-19 15:26:09 +0100

    product layout changes

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -10,37 +10,30 @@ const Product = ({ item, items, onAdd }) => {
   useEffect(() => console.log(isClicked), [isClicked]);
 
   return (
-    <div
-      className="card-container"
-      key={item.id}
-      onClick={() => setIsClicked(!isClicked)}
-    >
-      <div className="main-card">
-        <div className="card-img">
-          <img src={item.image} alt={item.name} />
-        </div>
-        <div className="card-name">
-          <p>{item.name}</p>
-        </div>
-        <div className="price-size">
-          <div className={item.inStock ? "in-stock" : "in-stock unavailable"}>
-            {item.inStock ? "in stock" : "unavailable"}
-          </div>
-          <div className="card-price">$ {item.price}.00</div>
-        </div>
-        <div>
-          {items.find((i) => i.id === item.id) ? (
-            <RemBtn onAdd={onAdd} newItem={item} />
-          ) : (
-            <AddBtn
-              onAdd={onAdd}
-              newItem={item}
-              classname={!item.inStock ? "disabled-btn" : "add-to-cart"}
-            />
-          )}
-        </div>
+    <div className="card-container">
+      <div className="card-img">
+        <img src={item.image} alt={item.name} />
       </div>
-      {isClicked && <div className="side-card">his</div>}
+      <div className="card-name">
+        <p>{item.name}</p>
+      </div>
+      <div className="price-size">
+        <div className={item.inStock ? "in-stock" : "in-stock unavailable"}>
+          {item.inStock ? "in stock" : "unavailable"}
+        </div>
+        <div className="card-price">$ {item.price}.00</div>
+      </div>
+      <div>
+        {items.find((i) => i.id === item.id) ? (
+          <RemBtn onAdd={onAdd} newItem={item} />
+        ) : (
+          <AddBtn
+            onAdd={onAdd}
+            newItem={item}
+            classname={!item.inStock ? "disabled-btn" : "add-to-cart"}
+          />
+        )}
+      </div>
     </div>
   );
 };
