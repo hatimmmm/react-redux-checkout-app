b6de36c1421355c8989d2cb97bfa6976b27ce66f
hatim <ourachhatim0@gmail.com>
2023-02-19 15:05:46 +0100

    expandable cards

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,42 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
+import RemBtn from "./rem-btn/RemBtn";
+import AddBtn from "./add-btn/AddBtn";
+import { useEffect } from "react";
+import "./product.css";
 
-const Product = ({ data }) => {
-  //   const { data } = useSelector((state) => state.cart);
+const Product = ({ item, items, onAdd }) => {
+  const [isClicked, setIsClicked] = useState(false);
+  useEffect(() => console.log(isClicked), [isClicked]);
+
   return (
-    <div>
-      {data.map((item) => {
-        return (
-          <div className="card-container" key={item.id}>
-            <div className="card-img">
-              <img src={item.image} alt={item.name} />
-            </div>
-            <div className="card-name">
-              <p>{item.name}</p>
-            </div>
-            <div className="price-size">
-              <div
-                className={item.inStock ? "in-stock" : "in-stock unavailable"}
-              >
-                {item.inStock ? "in stock" : "unavailable"}
-              </div>
-              <div className="card-price">$ {item.price}.00</div>
-            </div>
-            <div>
-              {items.find((i) => i.id === item.id) ? (
-                <RemBtn onAdd={onAdd} newItem={item} />
-              ) : (
-                <AddBtn
-                  onAdd={onAdd}
-                  newItem={item}
-                  classname={!item.inStock ? "disabled-btn" : "add-to-cart"}
-                />
-              )}
-              {}
-            </div>
+    <div
+      className="card-container"
+      key={item.id}
+      onClick={() => setIsClicked(!isClicked)}
+    >
+      <div className="main-card">
+        <div className="card-img">
+          <img src={item.image} alt={item.name} />
+        </div>
+        <div className="card-name">
+          <p>{item.name}</p>
+        </div>
+        <div className="price-size">
+          <div className={item.inStock ? "in-stock" : "in-stock unavailable"}>
+            {item.inStock ? "in stock" : "unavailable"}
           </div>
-        );
-      })}
+          <div className="card-price">$ {item.price}.00</div>
+        </div>
+        <div>
+          {items.find((i) => i.id === item.id) ? (
+            <RemBtn onAdd={onAdd} newItem={item} />
+          ) : (
+            <AddBtn
+              onAdd={onAdd}
+              newItem={item}
+              classname={!item.inStock ? "disabled-btn" : "add-to-cart"}
+            />
+          )}
+        </div>
+      </div>
+      {isClicked && <div className="side-card">his</div>}
     </div>
   );
 };
---
+++ src/components/product/add-btn/AddBtn.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./addBtn.css";
+
+import { motion } from "framer-motion";
+
+const AddBtn = ({ classname, onAdd, newItem }) => {
+  if (classname === "disabled-btn") {
+    return (
+      <button whileTap={{ scale: 0.8 }} className={classname}>
+        add to cart
+      </button>
+    );
+  } else {
+    return (
+      <motion.button
+        whileTap={{ scale: 0.8 }}
+        onClick={() => onAdd(newItem)}
+        className={classname}
+      >
+        add to cart
+      </motion.button>
+    );
+  }
+};
+
+export default AddBtn;
